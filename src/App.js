e9bd1a64c3022daba1285b1085648eb8ad2b1e0b
hadderaj <77488231+hadderaj@users.noreply.github.com>
2021-03-01 19:28:37 +0100

    test

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React from "react";
+import Deck from "./components/Deck";
+import HamburgerMenu from "./components/Menu";
+
+const App = () => <Deck />;
+
+
+
+
+// class App extends React.Component {
+// 	constructor() {
+// 		super();
+// 		this.state = {
+// 			open: [false, true, false, true]
+// 		};
+//   }
+// 	handleClick(id) {
+// 		let { open } = this.state;
+//     this.setState({
+// 			open: [...open.slice(0, id), !open[id], ...open.slice(id + 1)]
+// 		});
+// 	}
+// 	render() {
+// 		return (
+// 			<div className='menu-row'>
+// 				<HamburgerMenu
+// 					isOpen={this.state.open[3]}
+// 					menuClicked={this.handleClick.bind(this, 3)}
+// 					width={18}
+// 					height={15}
+// 					strokeWidth={1}
+// 					rotate={0}
+// 					color='black'
+// 					borderRadius={0}
+// 					animationDuration={0.5}
+// 				/>
+// 			  <HamburgerMenu
+// 		      isOpen={this.state.open[0]}
+// 		      menuClicked={this.handleClick.bind(this, 0)}
+// 		      width={36}
+// 					height={30}
+// 					strokeWidth={2}
+// 					rotate={0}
+// 		      color='black'
+// 					borderRadius={5}
+// 		      animationDuration={0.3}
+// 		    />
+// 				<HamburgerMenu
+// 					isOpen={this.state.open[1]}
+// 					menuClicked={this.handleClick.bind(this, 1)}
+// 					width={54}
+// 					height={45}
+// 					strokeWidth={3}
+// 					rotate={0}
+// 					color='black'
+// 					borderRadius={5}
+// 					animationDuration={0.4}
+// 				/>
+// 				<HamburgerMenu
+// 					isOpen={this.state.open[2]}
+// 					menuClicked={this.handleClick.bind(this, 2)}
+// 					width={72}
+// 					height={60}
+// 					strokeWidth={3}
+// 					rotate={0}
+// 					color='black'
+// 					borderRadius={5}
+// 					animationDuration={0.75}
+// 				/>
+// 			</div>
+// 		);
+// 	}
+
+
+
+export default App;
+
+
+//const App = () => <Deck />;
---
+++ src/data.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    pics: [
+      "https://picsum.photos/seed/picsum/200/300"
+    ],
+    name: "Berget",
+    age: 2400000,
+    distance: "3 miles away",
+    text:
+      "I like snow"
+  },
+  {
+    pics: [
+      "https://media-exp1.licdn.com/dms/image/C4E03AQFAq3-1pi5ohA/profile-displayphoto-shrink_200_200/0/1607853327476?e=1619654400&v=beta&t=rBf1S62Y62t-sM6eAVA3Bjecz3Ce0PCYzYZrAVWJ5Ik",
+      
+    ],
+    name: "Håkan",
+    age: 56,
+    distance: "2 miles away",
+    text:
+      "I like when your argurments have return statements"
+  },
+  {
+    pics: [
+      "http://3.bp.blogspot.com/-S9vTYolt_5s/T5ZHyqxhjaI/AAAAAAAAAW8/IaGiAxby4yo/s1600/pontus.jpg",
+      "https://th.bing.com/th/id/OIP.DrD0ZaOiUgmW6QkQs1D-swHaD2?w=325&h=180&c=7&o=5&dpr=1.25&pid=1.7"
+    ],
+    name: "Pontus",
+    age: 42,
+    distance: "1 mile away",
+    text: "Ill bring you to my dungeon of love!"
+  },
+  {
+    pics: [
+      "https://ca.slack-edge.com/T01G6J9JXDZ-U01GW7RCJ1E-002ae08c3992-512",
+      "https://runacademy.se/wp-content/uploads/2020/08/Dan-S%C3%A4len.jpg"
+    ],
+    name: "Dan",
+    age: 48,
+    distance: "running 4 miles away",
+    text:
+      "I don't do back end, but I could if you'd like"
+  }
+];
